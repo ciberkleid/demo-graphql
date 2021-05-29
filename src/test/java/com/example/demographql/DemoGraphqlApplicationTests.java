ddc835adb70f27a71889e1e76b6a2b090363e3c4
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-29 17:37:34 -0400

    corrected class name

--- src/test/java/com/example/demographql/DemoGraphqlApplicationTests.java
+++ src/test/java/com/example/demographql/DemoGraphqlApplicationTests.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class GraphqldemoApplicationTests {
+class DemoGraphqlApplicationTests {
 
 	@Autowired
 	MockMvc mockMvc;
